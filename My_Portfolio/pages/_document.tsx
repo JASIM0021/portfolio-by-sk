d1e355ddd66cf460bd59545baf5e6412cdc2662e
SK JASIMUDDIN <94641150+JASIM0021@users.noreply.github.com>
2022-08-19 15:29:15 +0530

    Add files via upload

---
+++ My_Portfolio/pages/_document.tsx
@@ -0,0 +1,17 @@
+import {Html,Head,NextScript, Main} from 'next/document'
+
+export default function Document(){
+
+  return(
+    <Html className='dark'>
+
+      <Head>
+      </Head>
+      <body>
+      <Main/>
+        <NextScript/>
+      </body>
+    
+    </Html>
+  )
+}
